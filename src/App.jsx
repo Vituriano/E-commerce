82d59c4045c9db3b99ef55418aa7cb0221d46ee7
Vituriano <viturianooliveira@gmail.com>
2020-01-12 23:59:43 -0400

    release/TagFilter: Segunda entrega. Filtro por tag e tags mais importantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import Background from './Views/Background/Background'
+import Products from './Views/Products/Products'
+import Header from './Views/Header/Header'
+import Aside from './Views/Aside/Aside'
 import './App.css'
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.handleQuery = this.handleQuery.bind(this);
         this.state = {
+            tag: '',
         };
     }
+    handleQuery(value) {
+        this.setState({
+            tag: value,
+        });
+    }
     render() {
+        const { tag } = this.state;
         return (
             <>
-                <Background />
+                <Header />
+                <div id="body">
+                    <Aside query={this.handleQuery}/>
+                    <Products tag={tag}/>
+                </div>
             </>
         );
     }
